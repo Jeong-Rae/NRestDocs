d2a5f619f1b8bcbec169989263ac3f80c0cc47ab
Jeong-Rae <kkwjdfo@gmail.com>
2025-04-03 01:45:00 +0900

    test: any 타입 린트 예외

--- src/core/writer/local-doc-writer.spec.ts
+++ src/core/writer/local-doc-writer.spec.ts
@@ -90,6 +90,7 @@ describe("local-doc-writer", () => {
             const content = "GET / HTTP/1.1";
             const expectedDir = path.join(config.outputDir, identifier);
 
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             (fs.access as any).mockRejectedValue(new Error("ENOENT: no such file or directory"));
 
             // When
