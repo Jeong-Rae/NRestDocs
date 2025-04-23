85a4c2fa083b8e00792dc509a29958af42ddae57
JRJR519 <kkwjdfo@gmail.com>
2025-04-24 05:11:46 +0900

    feat(response): response object 타입 추가

--- src/types/open-api-spec.ts
+++ src/types/open-api-spec.ts
@@ -1,11 +1,94 @@
 import type { JSONSchema } from "./json";
 
+interface Server {
+    url: string;
+    description?: string;
+    variables?: Record<string, ServerVariable>;
+}
+
+interface ServerVariable {
+    enum?: string[];
+    default: string;
+    description?: string;
+}
+
 interface Components {
     schemas: Record<string, Schema>;
+    responses: Record<string, Response | Reference>;
 }
 
 interface ExternalDocumentation {
     url: string;
+    description?: string;
+}
+
+interface Response {
+    description: string;
+    headers: Record<string, Header | Reference>;
+    content: Record<string, MediaType>;
+    links: Record<string, Link | Reference>;
+}
+
+interface MediaType {
+    schema: Schema;
+    example?: unknown;
+    examples?: Record<string, Example | Reference>;
+    encoding?: Record<string, Encoding>;
+}
+
+interface Encoding {
+    contentType: string;
+    headers: Record<string, Header | Reference>;
+    style: "string";
+    explode: boolean;
+    allowReserved: boolean;
+}
+
+interface Example {
+    summary?: string;
+    description?: string;
+    value?: unknown;
+    externalValue?: string;
+}
+
+interface Link {
+    operationRef?: string;
+    operationId?: string;
+    parameters?: Record<string, unknown>;
+    requestBody?: unknown;
+    description?: string;
+    server?: Server;
+}
+
+type Header = HeaderWithSchema | HeaderWithContent;
+
+interface HeaderBase {
+    description?: string;
+    required: boolean;
+    deprecated: boolean;
+}
+
+interface HeaderWithSchema extends HeaderBase {
+    style: "simple";
+    explode: boolean;
+    schema: Schema | Reference;
+    example?: unknown;
+    examples?: Record<string, Example | Reference>;
+    content: never;
+}
+
+interface HeaderWithContent extends HeaderBase {
+    content: Record<string, MediaType>;
+    style: never;
+    explode: never;
+    schema: never;
+    example: never;
+    examples: never;
+}
+
+interface Reference {
+    $ref: string;
+    summary?: string;
     description?: string;
 }
 
