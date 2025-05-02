0f7385c6872c12fee4d91ddb44b3171580da100f
JRJR519 <kkwjdfo@gmail.com>
2025-05-02 13:50:44 +0900

    feat(utils): 누락된 커버리지 보강

--- packages/core/src/utils/logger.spec.ts
+++ packages/core/src/utils/logger.spec.ts
@@ -9,6 +9,8 @@ describe("Logger", () => {
         vi.spyOn(console, "warn").mockImplementation(() => {});
         // biome-ignore lint/suspicious/noEmptyBlockStatements: Mock implementation does not require a body
         vi.spyOn(console, "error").mockImplementation(() => {});
+        // biome-ignore lint/suspicious/noEmptyBlockStatements: Mock implementation does not require a body
+        vi.spyOn(console, "debug").mockImplementation(() => {});
     });
 
     afterEach(() => {
@@ -31,5 +33,11 @@ describe("Logger", () => {
         const args = [new Error("Something went wrong"), "details"];
         Logger.error(...args);
         expect(console.error).toHaveBeenCalledWith("\x1b[31m[ERROR]\x1b[0m", ...args);
+    });
+
+    it("debug method should call console.debug", () => {
+        const args = ["Debug message", { key: "value" }];
+        Logger.debug(...args);
+        expect(console.debug).toHaveBeenCalledWith("\x1b[32m[DEBUG]\x1b[0m", ...args);
     });
 });
